80ac7c62fd6d139b6984f085bb7735729b90d4fb
marioluckiluke <bdfnatsu@gmail.com>
2023-08-21 14:01:13 +0700

    feat: added layout UI

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import SearchInput from '@/components/search-input';
+import { UserButton } from '@clerk/nextjs';
+import React from 'react';
+
+const RootPage = () => {
+  return (
+    <div className="h-full p-4 space-y-2">
+      <SearchInput />
+    </div>
+  );
+};
+
+export default RootPage;
